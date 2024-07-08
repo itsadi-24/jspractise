410618f969c8eb2b1ec8f8f5bb55a430ccf50e11
itsadi-24 <158060025+itsadi-24@users.noreply.github.com>
2024-07-08 11:03:36 +0530

    Prototype and Prototypal Inheritance in Javascript

---
+++ proto.js
@@ -0,0 +1,14 @@
+let object = {
+  name: 'adi',
+  age: 21,
+  city: 'paradip',
+};
+let arr = ['hi', 'hwllo'];
+let object2 = {
+  name: 'adipkay',
+};
+
+//now if we point proto of object2 to object
+// object2.__proto__ = object; //without this line its not possible
+// console.log(object2.age);
+//this concept is known as prototypal inheritance
